8a6ba46a631d0ed391bc1c13dbdce7d2181c557a
oakkitten <oakkitten@users.noreply.github.com>
2020-10-09 19:42:31 +0100

    add command to save only unnamed files; refactor things

--- save_unnamed.py
+++ save_unnamed.py
@@ -15,10 +15,12 @@ SETTINGS_FILE = "save_unnamed.sublime-settings"
 def log(*text):
     print(PLUGIN_NAME + ":", *text)
 
+
 def sanitize(text):
     text = re.sub(r"[^\w\-_\. {}()\[\]$=,]", "_", text)
     text = re.sub(r"\s+", " ", text)
     return text.strip()
+
 
 def get_line(view, n, max_chars=100):
     line = view.line(view.text_point(n, 0))
@@ -26,21 +28,25 @@ def get_line(view, n, max_chars=100):
         line = sublime.Region(line.begin(), line.begin() + max_chars)
     return view.substr(line)
 
+
 def get_first_line_with_text(view):
     for n in range(50):
         text = sanitize(get_line(view, n))
         if text:
             return text[:50]
 
+
 def get_extension_from_tmlanguage(data):
     data = plistlib.readPlistFromBytes(data.encode("utf-8"))
     return data['fileTypes'][0]
+
 
 # sublime-syntax is a yaml file;
 # avoid parsing the whole file with yaml as it's slow
 RE_FIRST_FILE_EXTENSION = re.compile(r"^file_extensions\s*:\s*[\r\n]*\s*-\s+(\w+)", re.M)
 def get_extesnion_from_sublime_syntax(data):
     return RE_FIRST_FILE_EXTENSION.search(data).group(1)
+
 
 @lru_cache(128)
 def get_extension_from_syntax_file(name):
@@ -53,9 +59,11 @@ def get_extension_from_syntax_file(name):
         import traceback; traceback.print_exc()
         return None
 
+
 def get_extension(view):
     syntax_file_name = view.settings().get('syntax')
     return get_extension_from_syntax_file(syntax_file_name) if syntax_file_name.startswith('Packages/') else None
+
 
 def assign_file_name_to_view(view, folder):
     date = datetime.now().strftime("%Y%m%d")
@@ -70,13 +78,18 @@ def assign_file_name_to_view(view, folder):
             return
     log("error: couldn't find a suitable file name like", full_name)
 
+
 def save_view(view, folder):
     had_file_name = bool(view.file_name())
-    if not had_file_name:
-        assign_file_name_to_view(view, folder)
 
-    log("existing:" if had_file_name else "new:     ", view.file_name())
+    if had_file_name:
+        log("saving a view into existing file:", view.file_name())
+    else:
+        assign_file_name_to_view(view, folder)
+        log("saving a view into a new file:", view.file_name())
+
     view.run_command("save")
+
 
 # joining the folder with "" puts an appropriate slash on the end of the folder
 # this prevents windows from wrongly reporting that a directory "foo " exists
@@ -84,22 +97,41 @@ def get_folder():
     folder = sublime.load_settings(SETTINGS_FILE).get("folder")
     folder = os.path.join(os.path.expanduser(folder), "")
     if not os.path.isdir(folder):
-        log("error: folder", folder, "doesn't exist")
-        sublime.error_message("{}: target folder \"{}\" doesn't exist".format(PLUGIN_NAME, folder))
-        return None
+        raise IOError("""folder "{}" doesn't exist""".format(folder))
     return folder
 
 
-class SaveAllFilesIncludingUnnamedCommand(sublime_plugin.ApplicationCommand):
+class SaveFiles(sublime_plugin.ApplicationCommand):
     def run(self):
-        log("getting folder...")
-        folder = get_folder()
-        if not folder:
-            return
+        try:
+            folder = get_folder()
+        except IOError as e:
+            sublime.error_message("{}: {}".format(PLUGIN_NAME, e))
+            raise
 
-        log("saving all files to {}...".format(folder))
+        log(self.saving_message.format(folder))
+
         for window in sublime.windows():
             for view in window.views():
-                if view.is_dirty():
-                    save_view(view, folder)
+                if not view.is_dirty():
+                    name = view.file_name()
+                    log("skipping a clean view:", "(no name)" if name is None else name)
+                    continue
+
+                if not self.save_if_has_name and view.file_name():
+                    log("skipping a dirty view that has a name:", view.file_name())
+                    continue
+
+                save_view(view, folder)
+
         log("done")
+
+
+class SaveAllFilesIncludingUnnamedCommand(SaveFiles):
+    saving_message = "saving all files, including unnamed, to {}..."
+    save_if_has_name = True
+       
+
+class SaveAllUnnamedFilesCommand(SaveFiles):
+    saving_message = "saving only unnamed files to {}..."
+    save_if_has_name = False
